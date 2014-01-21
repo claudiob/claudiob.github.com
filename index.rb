1544b52e6f1c010e65d22a4f5fdc037373478a06
claudiob <claudiob@gmail.com>
2014-01-21 11:34:21 -0800

    Add AListApart contribution

--- index.rb
+++ index.rb
@@ -9,8 +9,8 @@ user = load_user 'claudiob'
 
 repos = load_sources 'claudiob'
 is_fork = false
-File.open('index.html', 'w') {|f| f.puts erb.result}
+File.open('mine.html', 'w') {|f| f.puts erb.result}
 
 repos = load_forks 'claudiob'
 is_fork = true
-File.open('forks.html', 'w') {|f| f.puts erb.result}
+File.open('index.html', 'w') {|f| f.puts erb.result}
